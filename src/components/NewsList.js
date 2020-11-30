513202c498465c39b22c0a4a9c442909fcf17047
PratikGarai <pratikgarai0208@gmail.com>
2020-11-30 10:12:31 +0530

    basic list class

---
+++ src/components/NewsList.js
@@ -0,0 +1,11 @@
+import React,  {useState, useEffect} from 'react';
+
+const NewsList = (props) =>
+{
+    const newslist = useState([]);
+    return(
+        <div class="news-list">
+            
+        </div>
+    );
+}
