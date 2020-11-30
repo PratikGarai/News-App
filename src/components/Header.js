2478884d8d65579e60fc180cb45ec5f10cc36c88
PratikGarai <pratikgarai0208@gmail.com>
2020-11-30 20:05:53 +0530

    nav done

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import NewsList from './NewsList';
 import AboutComponent from './About';
+import '../css/Header.css';
 
 const Header = (props) =>{
     return (
         <header>
             <Router>
-                <Link to="">Home</Link><br />
-                <Link to="/about">About</Link>
+                <nav>
+                    <ul>
+                        <li><Link to="">
+                            <span class="links"><strong>Home</strong></span>
+                        </Link></li>
+                        <li><Link to="/about">
+                            <span class="links"><strong>About</strong></span>
+                        </Link></li>
+                    </ul>
+                </nav>
                 <Switch>
                     <Route path="/about">
                         <AboutComponent />
