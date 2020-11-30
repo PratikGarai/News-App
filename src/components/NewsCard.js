eb721c3f2949335337c45cf4a54594dbfb1b5aff
PratikGarai <pratikgarai0208@gmail.com>
2020-11-30 12:54:49 +0530

    css of cards properly made!

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -3,17 +3,23 @@ import React from 'react';
 const NewsCard = (props) =>{
     return (
         <div className="news-card col-md-5">
-            <div className="news-title">
-                <span className="title-content">
-                    <h2><strong>{props.news.title}</strong></h2>
-                </span>
+            <div class="news-card-image">
+                
             </div>
-            <div class="metadata">
-                <span className="author-name">{props.news.author}</span>
-                <span className="published-date">{props.news.published}</span>
-            </div>
-            <div className="content">
-                {props.news.content.substring(0,120)}...
+            <div class="news-card-text">
+                <div className="news-title">
+                    <span className="title-content">
+                        <h2><strong>{props.news.title}</strong></h2>
+                    </span>
+                </div>
+                <div class="metadata">
+                    <span className="author-name">{props.news.author+"  , "}</span>
+                    <span className="published-date">{props.news.published}</span>
+                </div>
+                <hr className="news-divider"/>
+                <div className="content">
+                    {props.news.content.substring(0,180)}...
+                </div>
             </div>
         </div>
         
